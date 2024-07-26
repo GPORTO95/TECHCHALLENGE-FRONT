ed6d06101aa35e4637ff4248ac03d7e42480b199
Gabriel Porto <gr_porto@hotmail.com>
2024-07-26 01:07:02 -0300

    feat (Contatos-list): Add botão para add e inputs que irão servir de filtros

--- src/app/contatos/contatos-list/contatos-list.component.ts
+++ src/app/contatos/contatos-list/contatos-list.component.ts
@@ -3,11 +3,12 @@ import { ContatosService } from '../contatos.service';
 import { Observable } from 'rxjs';
 import { ContatoList } from './contato-list';
 import { AsyncPipe } from '@angular/common';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contatos-list',
   standalone: true,
-  imports: [AsyncPipe],
+  imports: [AsyncPipe, ReactiveFormsModule ],
   templateUrl: './contatos-list.component.html',
   styleUrl: './contatos-list.component.scss'
 })
@@ -16,11 +17,26 @@ export class ContatosListComponent {
 
   contatos$ = new Observable<ContatoList[]>();
 
+  form = new FormGroup({
+    nome: new FormControl(''),
+    telefone: new FormControl(''),
+    email: new FormControl(''),
+    ddd: new FormControl('')
+  });
+
   constructor() {
     this.obterContatos();
   }
 
   private obterContatos() {
-    this.contatos$ = this.contatosService.obterContatos('');
+    //this.contatos$ = this.contatosService.obterContatos('');
+  }
+
+  onSubmitFiltros() {
+    console.log(this.form.value);
+  }
+
+  limparFiltros() {
+    this.form.reset();
   }
 }
